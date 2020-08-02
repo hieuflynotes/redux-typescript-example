bc1499a36c1973970c93abadaabda7c8770f2422
hieuflynotes <hieu@flynotes.co.uk>
2020-08-02 01:54:09 +0100

    change reducer

--- src/store/Reducer.ts
+++ src/store/Reducer.ts
@@ -1,14 +1,14 @@
 import { initialState, ApplicationState } from "./Store";
-import { Action } from "./Action";
+import { Action, ON_LOADING, OFF_LOADING } from "./Action";
 
 export const rootReducer = (
   state = initialState,
   action: Action
 ): ApplicationState => {
   switch (action.type) {
-    case "ON_LOADING":
+    case ON_LOADING:
       return { loading: true };
-    case "OFF_LOADING":
+    case OFF_LOADING:
       return { loading: false };
     default:
       return state;
